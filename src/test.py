1dd45a6021b8643f1d5c74ce5cece4deec96f747
Vencelusos <112923597+crazyninja77@users.noreply.github.com>
2026-02-18 20:39:18 +0100

    test.py

--- src/test.py
+++ src/test.py
@@ -1,15 +1,22 @@
-from qiskit import QuantumCircuit
-from qiskit_aer import AerSimulator
-from qiskit import transpile
+from qiskit import QuantumCircuit, Aer, transpile
+from qiskit.visualization import plot_histogram
 
+# 1 qubit in superposition
 qc = QuantumCircuit(1, 1)
 qc.h(0)
 qc.measure(0, 0)
 
-sim = AerSimulator()
+# Use simulator
+sim = Aer.get_backend('qasm_simulator')
 compiled = transpile(qc, sim)
+job = sim.run(compiled, shots=1024)
+result = job.result()
 
-result = sim.run(compiled, shots=1000).result()
 counts = result.get_counts()
+print("Counts:", counts)
 
-print("Result:", counts)
+
+try:
+    plot_histogram(counts).show()
+except:
+    pass
